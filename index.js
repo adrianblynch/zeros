578df532b1e1bf1053db1772a2be38a63fc5312b
Adrian Lynch <adrian.lynch@concrete.cc>
2015-04-21 19:04:13 +0100

    Allow number for second argument + Tests + Usage

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = function (number, mask) {
+
+	if (typeof mask === "number") {
+		mask = Array(mask + 1).join("0")
+	}
+
 	return (mask + number).slice(-Math.max(mask.length, (number + "").length));
 }
--- test/index.js
+++ test/index.js
@@ -22,6 +22,23 @@ describe('zeros', function () {
 
 	})
 
+	describe('with signature zeros(number, number)', function () {
+
+		it('should pad a number with the number of zeros given', function () {
+			var output = zeros(123, 4)
+			expect(output).to.equal("0123")
+		})
+
+		it('should leave number as-is when the number of digits equals that of the number given', function () {
+			var output = zeros(1234, 4)
+			expect(output).to.equal("1234")
+		})
+
+		it('should leave number as-is when the number of digits is more than that of the number given', function () {
+			var output = zeros(1234, 3)
+			expect(output).to.equal("1234")
+		})
+
 	})
 
 })
